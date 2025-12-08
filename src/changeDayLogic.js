d1ca4387b383475a494b5673b9f4334de2aa5a48
rasoulnasserifreelancer <rasoulnasseri_freelancer@yahoo.com>
2025-12-08 13:36:35 +0330

    i almost finished the desktop version of my app, now i'm pretty read to coninue working on tablet and mobile version

--- src/changeDayLogic.js
+++ src/changeDayLogic.js
@@ -3,18 +3,34 @@ import { getweatherInfoGotByUserLocation } from "./getLocationLogic.js";
 import { getweatherInfoGotByUserSearch } from "./searchLocationLogic.js";
 import { setHourlyWetherInfo } from "./setWeatherLogic.js";
 
-
-
 const currentDayElement = getHourlyWeatherElements().CurrentDayElement;
 
 currentDayElement.addEventListener("change", async (e) => {
-  let weatherInfo = getweatherInfoGotByUserSearch() || await getweatherInfoGotByUserLocation(e.target.dataset.currentLatitude, e.target.dataset.currentLlongitude) ;
-  console.log(weatherInfo);
-  for (let option of e.target) {
-    if (option.value === e.target.value) {
-      console.log(option);
-      option.selected = true;
-      setHourlyWetherInfo(weatherInfo[2]);
+  try {
+    let weatherInfo =
+      getweatherInfoGotByUserSearch() ||
+      (await getweatherInfoGotByUserLocation(
+        e.target.dataset.currentLatitude,
+        e.target.dataset.currentLlongitude
+      ));
+    console.log(weatherInfo, e.target);
+    for (let option of e.target) {
+      if (option.value === e.target.value) {
+        console.log(option);
+        option.selected = true;
+        setHourlyWetherInfo(weatherInfo[2]);
+      }
+    }
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      showErrorElement(`${error.message}`, "../assets/images/icon-retry.svg");
+    } else if (error instanceof TypeError) {
+      showErrorElement(
+        "network access error",
+        "../assets/images/icon-retry.svg"
+      );
+    } else {
+      showErrorElement(`${error.message}`, "../assets/images/icon-retry.svg");
     }
   }
 });
